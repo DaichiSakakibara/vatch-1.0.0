a83bd54767d9ad38377db70679a8171b17b9b710
DaichiSakakibara <dsdaitidsds1226@gmail.com>
2020-11-26 18:47:49 +0900

    フロントページデザイン作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname
+    validates :birth_day 
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :veg_id
+    validates :vatch_id
+    validates :sex_id
+    validates :blood_type_id
+    validates :prefecture_id
+  end
+
 end
