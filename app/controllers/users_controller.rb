cc965d53e741ff982f0063f4d2e594d2628c9247
DaichiSakakibara <dsdaitidsds1226@gmail.com>
2020-12-02 19:09:36 +0900

    ユーザーアイコンとvag属性表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,33 @@
 class UsersController < ApplicationController
 
   def index 
+    @users = User.all 
   end
-  
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    binding.pry
+    @user = User.find(params[:id])
+    if @user.id == current_user.id
+      @user.update(user_params)
+      flash[:success] = "プロフィールが更新されました！"
+      redirect_to user_path(@user)
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :avater)
+  end 
+
 end
